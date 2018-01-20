802535a8e273287d8f394086a4632a1efe475b4a
Agustin Siles <asiles@itx.com>
2018-01-20 11:24:31 -0300

    Insert rows to database

--- express/routes/products.js
+++ express/routes/products.js
@@ -3,22 +3,36 @@ const express = require('express'),
     sqlite3 = require('sqlite3').verbose(),
 	path = require('path'),
     dbFile = path.join(__dirname, '../data', 'husky.db'),
-	dbFileProd = path.join(process.resourcesPath, "express/data/husky.db");
+    dbFileProd = path.join(process.resourcesPath, "express/data/husky.db");
 
-const db = new sqlite3.Database(dbFileProd);
+let db = new sqlite3.Database(dbFile);
     
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS products (key TEXT, value INTEGER)");
-    db.run("INSERT INTO products (key, value) VALUES (?, ?)", "counter", 0);
+    db.run("CREATE TABLE IF NOT EXISTS products (name TEXT)");
 });
 
-router.get('/', (req, res) => res.json({ foo: 'bar' }));
+db.close();
 
-router.get('/all', (req, res) => {
-    db.get("SELECT * FROM products", (err, row) => {
-        if (err) return res.json({ err })
-        res.json({ row });
+router.get('/', (req, res) => {
+    db = new sqlite3.Database(dbFile);
+    db.serialize(() => {
+        db.all('SELECT name FROM products', (err, rows) => {
+            if (err) return res.json({ err });
+            return res.json({ rows })
+        });  
     });
+    db.close();
+});
+
+router.post('/add', (req, res) => {
+    db = new sqlite3.Database(dbFile);
+    db.serialize(() => {
+        db.run('INSERT INTO products VALUES (?)', [req.body.product], function getLastRow(err) {
+            if (err) return res.json(err);
+            return res.json({ product: req.body.product, id: this.lastID });
+        });
+    });
+    db.close();
 });
 
 module.exports = router;
